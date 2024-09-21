d722eedd8714c265c33efb9c2bc5bff1024cb843
amakawaMasahir0 <antegrreyalewy@gmail.com>
2024-09-21 19:57:14 +0800

    exercise 4 proxy_server.py update

--- exer4_proxy_server/src/proxy_server.py
+++ exer4_proxy_server/src/proxy_server.py
@@ -2,6 +2,8 @@
 # Only handle http text GET requests
 # cache algorithm is very simple, no outdated check
 ###
+# problem: i don't know why I can't create a new file and write it!!!!!! It drives me crazy for over 4 hours!!!!
+# solve: u must close a file after u use it!!!!! using with statement can avoid this disturbing problem!!!
 
 import os
 from socket import *
@@ -24,7 +26,7 @@ def html_header_gen(message, target_file, Error=False):
     completion.append("Content-Type: text/html; charset=UTF-8\r\n".encode())
     # NOTICE: content-Length is string number, not file size(os.path.filesize(file))
     # this can cause http error, and no html body would present
-    with open(target_file) as f:
+    with open(target_file, "r") as f:
         data = f.read()
     context_len = len(data)
     completion.append(f"Content-Length: {context_len}\r\n".encode())
@@ -53,7 +55,7 @@ while 1:
     message = tcpCliSock.recv(1024).decode()
     print(f"Request from {addr} is:\n{message}") # request from origin request client
     # Extract the filename from the given message
-    filename = message.split()[1].partition("/")[2]
+    filename = message.split()[1].partition("//")[2].replace("/", "_")
     filename = filename.replace("http://", "", 1)
     filename = filename.replace("www.", "", 1)
     print(f"Request filename is {filename}")
@@ -64,21 +66,20 @@ while 1:
     # print(file_path)
     try:
         # Check wether the file exist in the cache
-        f = open(file_path, "rb")
-        outputdata = f.read()
+        with open(file_path, "r") as f:
+            outputdata = f.read()
         fileExist = "true"
         # ProxyServer finds a cache hit and generates a response message
         cmpl_header = html_header_gen(message.encode(), file_path)
         for lines in cmpl_header:
             tcpCliSock.send(lines)
-        tcpCliSock.send(outputdata)
+        tcpCliSock.send(outputdata.encode())
         print('Read from cache.\n\n\n')
     except IOError:
         if fileExist == "false":
             # Create a socket on the proxyserver
             c = socket(AF_INET, SOCK_STREAM)
-            # hostn = "93.184.216.34"
-            hostn = filename.split('/')[0]
+            hostn = message.split()[1].partition("//")[2].partition("/")[0]
             print(f"Cache miss, trying fetch data from {hostn}:{internet_port}")
             try:
                 # Connect to the socket to port 80
@@ -91,8 +92,8 @@ while 1:
                 # Read the response into buffer
                 response_lines = fileobj.readlines()
                 headers = {}
-                for line in response_lines:
-                    print(line.decode())
+                # for line in response_lines:
+                #     print(line.decode())
                 for line in response_lines:
                     if line.__contains__(b"HTTP"):
                         continue
@@ -104,11 +105,11 @@ while 1:
                 print(f"Response Headers:\n{headers}")
                 # Create a new file in the cache for the requested file.
                 store_path = os.path.join(root_path, "html_cache", filename) + ".html"
-                tmpFile = open(store_path, "wb")
                 body_index = response_lines.index(b"\r\n") + 1
-                tmpFile.write("\n".encode())
-                for items in response_lines[body_index:]:
-                    tmpFile.write(items)
+                with open(store_path, "w") as f:
+                    f.write("\n")
+                    for items in response_lines[body_index:]:
+                        f.write(items.decode())
                 # Also send the response in the buffer to client socket and the corresponding file in the cache
                 cmpl_header = html_header_gen(b"", store_path)
                 for lines in cmpl_header:
